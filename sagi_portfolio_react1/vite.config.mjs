8f04492c1844e3927aed680b0a5a13cb7356b85a
Sagi-Sameah <sagisameah314@gmail.com>
2025-10-23 17:21:08 +0300

    Update vite.config.mjs

--- sagi_portfolio_react1/vite.config.mjs
+++ sagi_portfolio_react1/vite.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+server: {
+  https: true
+}
+
 export default defineConfig({
   plugins: [react()],
   build: { outDir: 'dist' }
